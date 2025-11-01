a0b51e1180838681639a0cbe35848c3dfe6c3c67
mariyum <mariyambintealkader@gmail.com>
2025-11-01 20:46:30 +0600

    Q1 solve

--- question1.dart
+++ question1.dart
@@ -1,18 +1,19 @@
 // Question 1: Basic Data Types & Functions (Difficulty: 1/5) ⭐
 // TODO: Complete the following requirements:
 
+
 // 1. Create variables of different data types: String, int, double, bool
 // TODO: Add your variables here
-String name = ""; // TODO: Initialize with your name
-int age = 0; // TODO: Initialize with your age
-double height = 0.0; // TODO: Initialize with your height
-bool isStudent = false; // TODO: Initialize with true or false
+String name = "Most Mariyum Binte Al Kader"; // TODO: Initialize with your name
+int age = 23; // TODO: Initialize with your age
+double height = 5.2; // TODO: Initialize with your height
+bool isStudent = true; // TODO: Initialize with true or false
 
 // 2. Write a function called calculateBMI that takes weight (double) and height (double) as parameters and returns the BMI as a double
 // TODO: Implement the calculateBMI function
 double calculateBMI(double weight, double height) {
-  // TODO: Calculate BMI = weight / (height * height)
-  return 0.0; // TODO: Replace with actual calculation
+   double BMI = weight / (height * height);
+  return BMI; // TODO: Replace with actual calculation
 }
 
 // 3. Write a function called getGrade that takes a score (int) and returns a grade (String) based on:
@@ -23,15 +24,27 @@ double calculateBMI(double weight, double height) {
 //    - Below 60: F
 // TODO: Implement the getGrade function
 String getGrade(int score) {
-  // TODO: Add your logic here
-  return ""; // TODO: Return appropriate grade
+  String grade;
+
+  if (score >= 90) {
+    grade = "A";
+  } else if (score >= 80) {
+    grade = "B";
+  } else if (score >= 70) {
+    grade = "C";
+  } else if (score >= 60) {
+    grade = "D";
+  } else {
+    grade = "F";
+  }
+  return grade; // TODO: Return appropriate grade
 }
 
 void main() {
   // TODO: Initialize your variables with appropriate values
-  name = "John Doe";
-  age = 25;
-  height = 5.9;
+  name = "Most Mariyum Binte Al Kader";
+  age = 23;
+  height = 5.2;
   isStudent = true;
   
   // TODO: Calculate BMI and grade
